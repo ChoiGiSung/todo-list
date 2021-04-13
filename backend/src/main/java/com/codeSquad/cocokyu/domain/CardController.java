e2f3657683186b36747f973b4a868d89411ae909
gisungchoi <chlrltjd159@naver.com>
2021-04-14 00:22:37 +0900

    refator : CardDto 추가
    - cardDto를 통해 값을 받는다.
    - cardDto를 통해 card생성이나 수정을 한다.

--- backend/src/main/java/com/codeSquad/cocokyu/domain/CardController.java
+++ backend/src/main/java/com/codeSquad/cocokyu/domain/CardController.java
@@ -1,10 +1,9 @@
 package com.codeSquad.cocokyu.domain;
 
+import com.codeSquad.cocokyu.domain.dto.CardDto;
 import com.codeSquad.cocokyu.domain.dto.CardList;
 import com.codeSquad.cocokyu.domain.dto.LogList;
 import com.codeSquad.cocokyu.domain.model.Card;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,7 +11,6 @@ import javax.validation.Valid;
 @RestController
 public class CardController {
 
-    Logger logger = LoggerFactory.getLogger(CardController.class);
     private CardService cardService;
 
     public CardController(CardService cardService) {
@@ -30,15 +28,16 @@ public class CardController {
     }
 
     @PostMapping("/todos")
-    public String create(@Valid Card card) {
+    public String create(@RequestBody @Valid CardDto cardDto) {
+        Card card = new Card(cardDto);
         cardService.write(card);
         //TODO : Response 객체 생성
         return "success";
     }
 
     @PutMapping("/todos/{id}")
-    public String update(@PathVariable Long id, @Valid Card updateCard) {
-        cardService.modify(id, updateCard);
+    public String update(@PathVariable Long id, @RequestBody @Valid CardDto cardDto) {
+        cardService.modify(id, cardDto);
         return "success";
     }
 
---
+++ backend/src/main/java/com/codeSquad/cocokyu/domain/dto/CardDto.java
@@ -0,0 +1,38 @@
+package com.codeSquad.cocokyu.domain.dto;
+
+import com.codeSquad.cocokyu.domain.model.Card.Status;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class CardDto {
+
+    @NotBlank
+    @NotNull
+    private final String title;
+
+    @NotBlank
+    @NotNull
+    private final String contents;
+
+    @NotNull
+    private final Status status;
+
+    public CardDto(String title, String contents, Status status) {
+        this.title = title;
+        this.contents = contents;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+}
